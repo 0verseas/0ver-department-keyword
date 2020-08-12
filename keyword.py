a96b270478f26f3567dff2c9324002ca07be51f8
hms5232 <hms5232@yahoo.com.tw>
2020-08-12 14:54:46 +0800

    產生一個切割後的 csv
    一列就是一個關鍵字，每一欄就是該關鍵字切割後的結果
    
    另外註解一些暫時用不到的功能

--- keyword.py
+++ keyword.py
@@ -3,9 +3,10 @@
 
 
 import os
+import csv
 
 import data_model
-from ckiptagger import WS, POS, NER  # 斷詞、詞性標記、命名實體識別
+from ckiptagger import WS, POS, NER  # 斷詞、詞性標記、命名實體辨識
 
 
 def main():
@@ -14,9 +15,30 @@ def main():
 		print("下載模型")
 		data_model.download()
 
-	# 範例用
-	ths = ['和我簽下契約，成為魔法少女吧', '魔法和奇蹟都是真實存在的', '那樣的話，大家不就只能去死了嗎', '已經沒什麼好害怕的了', '星光迴路遮斷器', '那是什麼，能吃嗎？', '你為什麼這麼熟練阿', '這麼可愛一定是男孩子', '不要瞎掰好嗎', '並沒有', '希望有一天，你能與珍愛的人重逢']
-	ckip(ths)
+	# 開始
+
+	origin_keywords = []
+	total_keywords = []
+
+	# 做點檢查是基本的
+	if os.path.isfile('./quota_keyword_search_log.csv'):
+		keywords_filename = './quota_keyword_search_log.csv'
+	else:
+		keywords_filename = input("請輸入欲分析的 csv 檔案：")
+
+	# 讀取關鍵字 csv
+	with open(keywords_filename, newline='') as csvf:
+		rows = csv.reader(csvf)
+		for row in rows:
+			origin_keywords.append(row[0])  # 將所有原始關鍵字都抽取出來
+
+	ws_keywords = ckip(origin_keywords)  # 次元切割刀
+
+	# 每列都是一個關鍵字切割後的結果的 csv
+	with open('origin_ws.csv', 'w', newline="") as ows:
+		writer = csv.writer(ows)
+		for keyword in ws_keywords:
+			writer.writerow(keyword)
 
 
 def ckip(keywords):
@@ -30,19 +52,21 @@ def ckip(keywords):
 
 	# 分析文本
 	ws_results = ws(keywords)
-	pos_results = pos(ws_results)
-	ner_results = ner(ws_results, pos_results)  # ner(文本, POS結果)
+	# pos_results = pos(ws_results)
+	# ner_results = ner(ws_results, pos_results)  # ner(文本, POS結果)
 
 	# 結果
-	print(ws_results)  # 斷詞
-	print(pos_results)  # 詞性
-	for name in ner_results[0]:  # 實體辨識
-	    print(name)
+	# print(ws_results)  # 斷詞
+	# print(pos_results)  # 詞性
+	# for name in ner_results[0]:  # 實體辨識
+	#     print(name)
 
 	# release memory
 	del ws
 	del pos
 	del ner
+
+	return ws_results
 
 
 if __name__ == '__main__':
